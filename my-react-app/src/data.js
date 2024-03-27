ea20cfbed31f40e5ef6f4436d01d85f34bf2630a
Deepak <deepakdeepu3025@gmail.com>
2024-03-27 23:49:09 +0530

    added

---
+++ my-react-app/src/data.js
@@ -0,0 +1,7 @@
+const Products = [
+    {id: 1, name: 'Product-1', price:100, quatitySelected : 0},
+    {id: 2, name: 'Product-2', price:200, quatitySelected : 0},
+    {id: 3, name: 'Product-3', price:300, quatitySelected : 0},
+];
+    
+export default Products;
